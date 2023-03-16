202a208b03de0c7cdff5045b82c32ebe969c090c
Gutmanis <nauirssgg@gmail.com>
2023-03-16 08:32:16 +0200

    Izveido izvēļu variantus kas bus pieejami sakuma

---
+++ src/pasutijums.java
@@ -0,0 +1,13 @@
+
+	class Order {
+		   private String name;
+		   private String phone;
+		   private String address;
+		   private String size;
+		   private String[] toppings;
+		   private String sauce;
+		   private double price;
+		   private boolean delivery;
+		private String dzeriens;
+	}
+
---
+++ src/pica.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class pica {
+	   public static void main(String[] args) {
+	      //Izveido array lai saglabatu sutijumus
+	      Order[] orders = new Order[10];
+	      int orderCount = 0;
+	      
+	    	  String[] izvelnesVarianti = {"Izveidot jaunu pasutijumu", "Apskatīt izveidotas picas","Kārtot picas augošā/dilstošā secībā pēc cenas","Nodot picu klientam", "Iziet no programmas"};
+	    	String izvelesVariantuNosaukumi;
+	   }
+}
